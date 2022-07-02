57dd4d2c34bc51e05868dcae65be6533474b73fb
aditya1746 <68777899+aditya1746@users.noreply.github.com>
2022-07-03 02:31:05 +0530

    Update surface_straight.py

--- surface_straight.py
+++ surface_straight.py
@@ -8,6 +8,10 @@ from std_msgs.msg import Int32
 from std_msgs.msg import Float64
 from std_msgs.msg import String
 
+
+# 	  	   FL BL  FR BR
+# straight CW CCW CW CCW
+
 yaw = 0
 acc_y = 0
 prev_err_y, int_err_y, prev_err_a, int_err_a = 0,0,0,0
@@ -18,9 +22,13 @@ err_a_thresh = 0
 turning_factor = 0
 MAXpwm_fr, MAXpwm_fl, MAXpwm_br, MAXpwm_bl, MAXpwm_mr, MAXpwm_ml = 1900, 1900, 1900, 1900, 1900, 1900
 MINpwm_fr, MINpwm_fl, MINpwm_br, MINpwm_bl, MINpwm_mr, MINpwm_ml = 1100, 1100, 1100, 1100, 1100, 1100
-pwm_fr, pwm_br, pwm_fl, pwm_bl, pwm_mr, pwm_ml = 1600, 1600, 1600, 1600, 1500, 1500 
+# pwm_fr, pwm_br, pwm_fl, pwm_bl, pwm_mr, pwm_ml = 1600, 1400, 1600, 1400, 1500, 1500 # ========== base velocities
+pwm_fr, pwm_br, pwm_fl, pwm_bl, pwm_mr, pwm_ml = 100, -100, 100, -100, 0 , 0  # ================== 100 basically represent base velocities
 
-#err_y is positive => robot is turned towards right, when we are looking from back
+# x-axis ^ 
+# y-axis >
+# err_y is positive => robot is turned towards right, when we are looking from back
+# err_a is positive => robot is moving in y axis
 
 def straightLine_pid_imu():
 	global prev_err_y, int_err_y, prev_err_a, int_err_a
@@ -47,37 +55,35 @@ def straightLine_pid_imu():
 	 	correction_a =correction_a - KI_a*int_err_a 
 
 	if(err_y > err_y_thresh):
-		pwm_fr += correction_y #front right should rotate in a way such that one component of it's force is towards left
-		pwm_bl += correction_y - turning_factor #to go forward and at the same time turn slightly left
-
+		pwm_fr += correction_y # increasing the force of front right
+# 		pwm_bl += correction_y # decreasing the force of back left, indeed we could change any of the 4ci, so for the simplicity lets change only one
 		pwm_fr = min(pwm_fr, MAXpwm_fr)
-		pwm_fr = max(pwm_fr, MINpwm_fr)
-		pwm_bl = min(pwm_bl, MAXpwm_bl)
-		pwm_bl = max(pwm_bl, MINpwm_bl)
+		pwm_fr = max(pwm_fr, 100) 
+# 		pwm_bl = min(pwm_bl, 0)
+# 		pwm_bl = max(pwm_bl, MINpwm_bl)
 	
 	elif(err_y < -err_y_thresh):
 		pwm_fl += correction_y
-		pwm_br += correction_y - turning_factor
+# 		pwm_br += correction_y
 		pwm_fl = min(pwm_fl, MAXpwm_fl)
-		pwm_fl = max(pwm_fl, MINpwm_fl)
-		pwm_br = min(pwm_br, MAXpwm_br)
-		pwm_br = max(pwm_br, MINpwm_br)
+		pwm_fl = max(pwm_fl, 100)
+# 		pwm_br = min(pwm_br, 0)
+# 		pwm_br = max(pwm_br, MINpwm_br)
 	
-	elif(abs(err_a) > err_a_thresh):
+	elif(abs(err_a) > err_a_thresh):  #what if, auv is facing straight but moving left or right or diagnonally. 
 		if(err_a>0):
-			pwm_fr += correction_a
-			pwm_bl += correction_a
-
+			pwm_fr += correction_a # increase the force of front right and back left
+			pwm_bl -= correction_a # and yes here we need to change both or all 4, so that no turning force is created
 			pwm_fr = min(pwm_fr, MAXpwm_fr)
-			pwm_fr = max(pwm_fr, MINpwm_fr)
-			pwm_bl = min(pwm_bl, MAXpwm_bl)
+			pwm_fr = max(pwm_fr, 100)
+			pwm_bl = min(pwm_bl, 0)
 			pwm_bl = max(pwm_bl, MINpwm_bl)
 		else:
 			pwm_fl += correction_a
-			pwm_br += correction_a
+			pwm_br -= correction_a
 			pwm_fl = min(pwm_fl, MAXpwm_fl)
-			pwm_fl = max(pwm_fl, MINpwm_fl) 
-			pwm_br = min(pwm_br, MAXpwm_br)
+			pwm_fl = max(pwm_fl, 100) 
+			pwm_br = min(pwm_br, 0)
 			pwm_br = max(pwm_br, MINpwm_br)
 			
 	pwm_msg = str(pwm_fr) + ' ' + str(pwm_fl) + ' ' + str(pwm_mr) + ' ' + str(pwm_ml) + ' ' + str(pwm_br) + ' ' + str(pwm_bl) + ' '
