a41531b9159ce7d264a24a894dad19c1c7403888
Dev Raj Jain <dev_rj@ece.iitr.ac.in>
2022-07-15 00:23:10 +0530

    Create only_depth.py

--- only_depth.py
+++ only_depth.py
@@ -1,113 +1,76 @@
 #! /usr/bin/env python
-from numpy import float32
+
+# from sympy import true
 import rospy
+
 from dynamic_reconfigure.server import Server
-#from auv_codes2.cfg import thrusterConfig
-from auv_codes2.cfg import PID_yawConfig
+from auv_codes2.cfg import thrusterConfig
 from std_msgs.msg import Int32MultiArray
 from std_msgs.msg import Int32
-from std_msgs.msg import Float64
-from std_msgs.msg import Float32
 from std_msgs.msg import String
 
 
-#    FL BL FR BR ML MR
-# F  C  C  C  C 
-# B  A  A  A  A
-# L  A  C  C  A
-# R  C  A  A  C
-# D  -  -  -  -  C  C
-# U  -  -  -  -  A  A
-
-prev_err_d, int_err_d = 0,0         # d -> depth
-err_d_thresh = 0.007                # meters
-prev_err_r, int_err_r = 0,0         # r -> roll
-err_r_thresh = 10                   # degrees
-
-depth = 0
-desiredDepth = 0.7                  # meters
-roll = 0
-
-KP_d, KD_d, KI_d = 1,1,1
-
-MAXpwm, MINpwm = 400, -400
 pwmBase = 1470
-pwm_mr, pwm_ml = 100 , 100  # ================== 100 basically represent base velocities (1470)
 
-# x-axis ^ 
-# y-axis >
+def callback_verify(data):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
 
-def depth_pid_pressure():
-	global prev_err_d, int_err_d
-	global pwm_mr, pwm_ml
-	# pwm_mr, pwm_ml = 100, 100  # ================== 100 basically represent base velocities
-	
-	err_d = desiredDepth - depth # +err => or niche jana he
-	diff_err_d = err_d - prev_err_d
-	int_err_d += err_d 
-    prev_err_d = err_d
-	
-	correction_d = KP_d*abs(err_d) + KD_d*diff_err_d
-	if(int_err_d*err_d > 0) :
-		correction_d = correction_d + KI_d*int_err_d 
-	else:
-	 	correction_d =correction_d - KI_d*int_err_d 
-	correction_d = max(correction_d,0)
-	
-	if(abs(err_d) > err_d_thresh):
-		pwm_mr, pwm_ml = 100, 100
 
-	if(err_d > err_d_thresh):
-		pwm_mr += correction_d # increasing the force of both thrusters
-        pwm_ml += correction_d
-        pwm_mr = min(pwm_mr, MAXpwm)
-        pwm_ml = min(pwm_ml, MAXpwm)
-	
-	elif(err_y < -err_y_thresh):
-		pwm_ml -= correction_d
-        pwm_mr -= correction_d
-        pwm_mr = max(pwm_mr, MINpwm)
-        pwm_ml = max(pwm_ml, MINpwm)
-	
-	elif(abs(err_r) > err_r_thresh):  # what if, auv is rolled towards one side, which is really rare, thus keeping it simple
-		if(err_r>0): # \ this back slash represents the line joining middle right and left thruster when seen from back 
-			pwm_ml += err_r
-            pwm_ml = min(pwm_ml, MAXpwm)
-		else:
-            pwm_mr += err_r
-            pwm_mr = min(pwm_mr, MAXpwm)
-			
-	pwm_mr, pwm_ml = pwmBase + int(pwm_mr), pwmBase - 1*int(pwm_ml)
 
-	pwm_msg = str(pwm_mr) + ' ' + str(pwm_ml) + ' '
-	pub.publish(pwm_msg)
-	rospy.loginfo(depth)
-	
+def callback_conf(config, level):
+    rospy.loginfo("""Reconfigure Request: {servoFrontRight}, {servoFrontLeft},{servoMiddleRight}, {servoMiddleLeft}, {servoBackLeft}, {servoBackRight},{mode_thrusters}""".format(**config))
+   
+    mode   = config.mode_thrusters
+    pwm_fr, pwm_br, pwm_fl, pwm_bl, pwm_mr, pwm_ml =0, 0, 0, 0, 0, 0
+    if (mode==0):
+            #stop all thrusters
+        print('mode is ', mode)
+        pwm_fr, pwm_br, pwm_fl, pwm_bl, pwm_mr, pwm_ml =0, 0, 0, 0, 0, 0
 
-def callback_gui(config, level):
-    rospy.loginfo("""Reconfigure Request: {KP_depth}, {KI_depth}, {KD_depth}""".format(**config))
-    global KP_d, KI_d, KD_d
-    KP_d, KI_d, KD_d = config.KP_depth / 100, config.KI_depth / 100, config.KD_depth / 100 
+    elif  (mode==1):
+               #all thrusters value to 1600 to check every thruster is working
+        print('mode is ', mode)   
+        pwm_fr, pwm_br, pwm_fl, pwm_bl, pwm_mr, pwm_ml =80, 80, 80, 80, 80, 80
+
+    elif  (mode==2):  
+        
+              # value receieved from reconfigure
+        print('mode is ', mode)
+        pwm_fr = config.servoFrontRight
+        pwm_fl = config.servoFrontLeft
+        pwm_mr = config.servoMiddleRight
+        pwm_ml = config.servoMiddleLeft
+        pwm_br = config.servoBackRight
+        pwm_bl = config.servoBackLeft
+
+    elif  (mode==3): 
+        print('mode is ', mode)
+
+    elif  (mode==4):         # value from imu sensor 
+
+        print('mode is ', mode)
+     
+        
+    elif  (mode==5):        # value from camera 
+        print('mode is ', mode)
+
+    pwm_fr, pwm_br, pwm_fl, pwm_bl , pwm_mr, pwm_ml = pwmBase - int(pwm_fr), pwmBase +int(pwm_br),pwmBase + int(pwm_fl), pwmBase +int(pwm_bl), pwmBase + int(pwm_mr), pwmBase - int(pwm_ml)
+    
+    pwm_value= str(pwm_fr) +' '+ str(pwm_fl) +' '+str(pwm_mr)+ ' '+ str(pwm_ml)+ ' '+ str(pwm_br)+' '+ str(pwm_bl)
+    pub.publish(pwm_value) 
+
+    print("Out of if else")
+    
+           
+    # pub7.publish(stop)
+
+
     return config
 
-def depth_callback(msg):
-	global depth
-	depth = msg.data
-    depth_pid_pressure()
-
-def roll_callback(msg):
-	global roll
-	roll = msg.data
-
 if __name__ == "__main__":
-	
-    rospy.init_node("only_depth_pid", anonymous = False)
+    rospy.init_node("thruster_value", anonymous = False)
     q = 1
-    
-    rospy.Subscriber("Depth", Float32, depth_callback)
-    rospy.Subscriber("angle_x", Float64, roll_callback)
-	
-    pub=rospy.Publisher("PWM_VALUE_Middle",String ,queue_size=q)
-    srv = Server(PID_yawConfig, callback_gui)
-   
+    pub=rospy.Publisher("PWM_VALUE",String ,queue_size=q)
+    rospy.Subscriber("Verify", String, callback_verify)
+    srv = Server(thrusterConfig, callback_conf)
     rospy.spin()
